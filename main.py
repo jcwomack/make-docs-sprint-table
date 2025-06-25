e76880d904e0ed003b300d30fb080b7dd7fa2bfd
James C. Womack <J.C.Womack@bristol.ac.uk>
2025-06-25 15:23:53 +0100

    Format

--- main.py
+++ main.py
@@ -7,15 +7,19 @@ from typing import Generator
 import pandas as pd
 import yaml
 
+
 def walk_markdown_files(root_dir: Path) -> Generator[Path]:
     for dirpath, _, filenames in root_dir.walk():
         for filename in filenames:
-            if filename.lower().endswith(('.md', '.markdown')):
+            if filename.lower().endswith((".md", ".markdown")):
                 yield Path(dirpath, filename)
+
 
 def extract_title(md_text: str) -> str:
     # Use frontmatter YAML
-    match = re.search(r"^---$(?P<yaml>.*?)^---$", md_text, flags=re.MULTILINE | re.DOTALL)
+    match = re.search(
+        r"^---$(?P<yaml>.*?)^---$", md_text, flags=re.MULTILINE | re.DOTALL
+    )
 
     if match:
         try:
@@ -33,23 +37,33 @@ def extract_title(md_text: str) -> str:
 
     if match:
         return match.group("title")
-    
+
     return None
+
 
 def make_dataframe(root_dir: Path) -> pd.DataFrame:
 
     paths = list(walk_markdown_files(root_dir))
 
-    path_series = pd.Series(map(lambda p: str(p.relative_to(root_dir)), paths), dtype="string", name="Path")
-    title_series = pd.Series(map(lambda p: extract_title(p.read_text()), paths), dtype="string", name="Title")
+    path_series = pd.Series(
+        map(lambda p: str(p.relative_to(root_dir)), paths), dtype="string", name="Path"
+    )
+    title_series = pd.Series(
+        map(lambda p: extract_title(p.read_text()), paths), dtype="string", name="Title"
+    )
 
     # Red-Amber-Green assignment: default "R" at start of sprint
     rag_series = pd.Series("R", dtype="string", index=path_series.index, name="R-A-G")
 
     # Assignee: default empty at start of sprint
-    assignee_series = pd.Series(pd.NA, dtype="string", index=path_series.index, name="Assignee")
+    assignee_series = pd.Series(
+        pd.NA, dtype="string", index=path_series.index, name="Assignee"
+    )
 
-    return pd.concat([path_series, title_series, rag_series, assignee_series], axis="columns")
+    return pd.concat(
+        [path_series, title_series, rag_series, assignee_series], axis="columns"
+    )
+
 
 def main(root_dir: Path, output_csv: Path | None):
 
@@ -62,11 +76,22 @@ def main(root_dir: Path, output_csv: Path | None):
 
     df.to_csv(output_csv, index_label="Id")
 
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("root_dir", type=Path, help="Root directory to recursively search for Markdown documents")
-    parser.add_argument("output_csv", type=Path, default=None, nargs='?', help="Optional path for output to CSV file (if not provided, output to stdout)")
+    parser.add_argument(
+        "root_dir",
+        type=Path,
+        help="Root directory to recursively search for Markdown documents",
+    )
+    parser.add_argument(
+        "output_csv",
+        type=Path,
+        default=None,
+        nargs="?",
+        help="Optional path for output to CSV file (if not provided, output to stdout)",
+    )
     args = parser.parse_args()
 
     main(args.root_dir, args.output_csv)
